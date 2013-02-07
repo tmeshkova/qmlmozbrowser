9fe58b8b9eea258ef97b5d537770f22ef86d156f
Tatiana Meshkova <tanya.meshkova@gmail.com>
2013-02-06 18:45:22 -0800

    Fix obdir detection

--- link_to_system.sh
+++ link_to_system.sh
@@ -6,12 +6,8 @@ if [ "$TARGET_DIR" = "" ]; then
   TARGET_DIR=/usr/bin
 fi
 
-OBJDIR="obj--dir"
-
-if [ -f $(pwd)/objdir-name ]; then
-  OBJDIR=`cat $(pwd)/objdir-name`;
-  echo "objdir-stamp detected: $OBJDIR"
-fi
+BARCH=`uname -m`
+OBJDIR="obj-$BARCH-dir"
 
 mkdir -p $TARGET_DIR
 
